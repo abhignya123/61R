89470f9ddfd68d2df0619185c86c526af9835cff
Abhignya Nadakuditi <117672157+abhignya123@users.noreply.github.com>
2026-01-22 15:21:28 +0530

    Add files via upload

---
+++ s.js
@@ -0,0 +1,50 @@
+let str="prabha";
+let newstr=str.toUpperCase()
+console.log(newstr)
+// let str="RISHI"
+// let newstr=str.toLowerCase()
+// console.log(newstr)
+// let name="    Jhanshi Iddumani   "
+// let newname=name.trim()
+// console.log(newname)
+// let str="      nikitha Rekha"
+// console.log(str.trimStart())
+// let str="nikitha rekha      "
+// console.log(str.trimEnd())
+// let str="Srujana"
+// console.log(str.slice(2))
+// console.log(str.slice(0,3))
+// console.log(str.slice(4,-1))
+// console.log(str.slice(-4,-1))
+// console.log(str.slice())
+// console.log(str.slice(5))
+// let str1="srujana"
+// let str2="ram"
+// console.log(str1.concat(str2))
+// console.log(str1+str2)
+// console.log(str1+" "+str2)
+// let str="hello"+123
+// console.log(str)
+// console.log(typeof str)
+// let num=123
+// console.log(typeof num)
+// let str="Himanth"
+// console.log(str.replace("H","J"))
+// let str="Amulya"
+// console.log(str.replace("A",4))
+// console.log(str.replace("a","6"))
+// let str="prabha"
+// console.log(str.charAt(3))
+// console.log(str.charAt(2))
+// let str="savithri"
+// console.log(str.indexOf("t"))
+// console.log(str.indexOf("s"))
+// let str="hello hello"
+// console.log(str.lastIndexOf("e"))
+// let str="java script"
+// console.log(str.includes("script"))
+// console.log(str.includes("java"))
+// console.log(str.includes("python"))
+// console.log(str.startsWith("java"))
+// console.log(str.endsWith("script"))
+// console.log(str.endsWith("pt"))
